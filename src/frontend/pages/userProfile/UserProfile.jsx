10370d0517265f43abfc33e040d28dc4b909358a
kashifhussainpathan <121045014+kashifhussainpathan@users.noreply.github.com>
2023-07-09 17:07:01 +0530

    Refactored

--- src/frontend/pages/userProfile/UserProfile.jsx
+++ src/frontend/pages/userProfile/UserProfile.jsx
@@ -3,9 +3,8 @@ import "./userProfileStyles.css";
 import { useContext } from "react";
 import { PostContext } from "src/frontend/context/post-context";
 import { UserContext } from "src/frontend/context/user-context";
-
+import { UserPosts } from "src/frontend/components/UserPosts";
 import { ProfileComponent } from "src/frontend/components/profileComponent/ProfileComponent";
-import { UserPosts } from "src/frontend/components/userPosts";
 
 export const UserProfile = () => {
   const {
@@ -24,8 +23,10 @@ export const UserProfile = () => {
     <>
       <h3 className="page-header"> {userProfile.username}</h3>
       <hr className="post-break-hr" />
+
       <section className="user-profile-section">
         {/* Profile Details */}
+
         <ProfileComponent userProfile={userProfile} />
 
         {/* Users Posts Header */}
