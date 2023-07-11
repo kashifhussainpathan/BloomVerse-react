63039aa2763b75d592ada1a356446f48d5569552
kashifhussainpathan <121045014+kashifhussainpathan@users.noreply.github.com>
2023-07-11 14:14:55 +0530

    style: added active icon

--- src/frontend/components/bottomNav/BottomNav.jsx
+++ src/frontend/components/bottomNav/BottomNav.jsx
@@ -1,11 +1,14 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import "./bottomNav.css";
 
 // import react-icons;
-import { MdOutlineExplore } from "react-icons/md";
+import { RiBookmarkFill } from "react-icons/ri";
 import { HiOutlineBookmark } from "react-icons/hi";
-import { AiOutlineHome, AiOutlinePlusCircle } from "react-icons/ai";
-import { useContext } from "react";
+import { MdOutlineExplore, MdExplore } from "react-icons/md";
+import { AiOutlineHome, AiOutlinePlusCircle, AiFillHome } from "react-icons/ai";
+
+// Importing context
 import { UserContext } from "src/frontend/context/user-context";
 import { PostContext } from "src/frontend/context/post-context";
 
@@ -20,26 +23,36 @@ export const BottomNav = () => {
     <aside className="bottom-nav">
       <div>
         <NavLink to="/">
-          <AiOutlineHome className="home-bold-icon" />
+          {location.pathname === "/" ? (
+            <AiFillHome className="home-bold-icon" />
+          ) : (
+            <AiOutlineHome className="home-bold-icon" />
+          )}
         </NavLink>
       </div>
 
       <div>
         <NavLink to="/explore">
-          <MdOutlineExplore className="explore-icon" />
+          {location.pathname === "/explore" ? (
+            <MdExplore className="explore-icon" />
+          ) : (
+            <MdOutlineExplore className="explore-icon" />
+          )}
         </NavLink>
       </div>
 
       {/* Add new post button */}
       <div onClick={() => setIsCreatePostModalOpen(true)}>
-        {" "}
-        <AiOutlinePlusCircle className="home-bold-icon" />{" "}
+        <AiOutlinePlusCircle className="home-bold-icon" />
       </div>
 
       <div>
         <NavLink to="/bookmark">
-          {" "}
-          <HiOutlineBookmark />
+          {location.pathname === "/bookmark" ? (
+            <RiBookmarkFill />
+          ) : (
+            <HiOutlineBookmark />
+          )}
         </NavLink>
       </div>
 
